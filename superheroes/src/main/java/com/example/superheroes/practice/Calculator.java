cc6f463db80185fcd84ced890c8b0bf664e91dc5
Simon <simon.benkner@gmx.de>
2023-07-28 10:38:27 +0100

    Finished Springboot QL-2 Optional

--- superheroes/src/main/java/com/example/superheroes/practice/Calculator.java
+++ superheroes/src/main/java/com/example/superheroes/practice/Calculator.java
@@ -1,12 +1,26 @@
 package com.example.superheroes.practice;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Calculator {
 
+    @PostConstruct
+    public void init() {
+        System.out.println("Calculator is created");
+    }
+
     public int addition(int a, int b) {
         return a+b;
     }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Calculator is destroyed");
+    }
+
+
 
 }
