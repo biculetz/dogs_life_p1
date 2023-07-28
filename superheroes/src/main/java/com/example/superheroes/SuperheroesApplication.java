d56ccf9f471b6341069cbea500bd7cbc223e084e
Simon <simon.benkner@gmx.de>
2023-07-28 13:05:19 +0100

    Added SpringBoot QL-4

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,16 +1,19 @@
 package com.example.superheroes;
 
-import com.example.superheroes.practice.Calculator;
+import com.example.superheroes.model.Hero;
+import com.example.superheroes.repository.HeroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SuperheroesApplication implements CommandLineRunner {
 
 	@Autowired
-	// Calculator calc = new Calculator();
+	HeroRepository hr;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
@@ -18,8 +21,10 @@ public class SuperheroesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
-		// System.out.println(calc.addition(10,5));
-
+		List<Hero> heroes = hr.findHeroesNameStartingWithLetter("J%");
+		System.out.println("Query Result Length: " + heroes.size());
+		for(Hero hero : heroes) {
+			System.out.println(hero);
+		}
 	}
 }
