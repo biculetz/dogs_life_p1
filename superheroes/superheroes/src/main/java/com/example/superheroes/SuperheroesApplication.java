cc4f0ae810881c1ae2a81a71b15f756bf141d748
Fabian t8 <branzeafabian21@gmail.com>
2023-07-28 13:44:44 +0100

    ql 6

--- superheroes/superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -12,25 +12,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
 
 @SpringBootApplication
-public class SuperheroesApplication implements CommandLineRunner {
+public class SuperheroesApplication  {
 
-	@Autowired
-	HeroService heroService;
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
 
-		List<Hero> heroes = heroService.getAllHeros();
-
-
-		for (Hero hero : heroes){
-			System.out.println(hero);
-		}
-
-		System.out.println("Hello World!");
-
-	}
 }
