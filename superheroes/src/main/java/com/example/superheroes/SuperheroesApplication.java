a62f7d4461423e452b40372b85dfd32872ec8066
Simon <simon.benkner@gmx.de>
2023-07-28 10:29:32 +0100

    Added Springboot QL-2

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,11 +1,16 @@
 package com.example.superheroes;
 
+import com.example.superheroes.practice.Calculator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SuperheroesApplication implements CommandLineRunner {
+
+	@Autowired
+	Calculator calc = new Calculator();
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
@@ -14,5 +19,6 @@ public class SuperheroesApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello World");
+		System.out.println(calc.addition(10,5));
 	}
 }
---
+++ superheroes/src/main/java/com/example/superheroes/practice/Calculator.java
@@ -0,0 +1,12 @@
+package com.example.superheroes.practice;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+
+    public int addition(int a, int b) {
+        return a+b;
+    }
+
+}
