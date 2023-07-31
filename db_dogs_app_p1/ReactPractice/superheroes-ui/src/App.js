2acad038f9ec376726effc138b0df28418030e4c
Fabian t8 <branzeafabian21@gmail.com>
2023-07-31 16:02:44 +0100

    React ql

---
+++ db_dogs_app_p1/ReactPractice/superheroes-ui/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import AllHeroes from './components/AllHeroes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+
+function App() {
+  return (
+    <AllHeroes/>
+    
+  );
+}
+
+export default App;
