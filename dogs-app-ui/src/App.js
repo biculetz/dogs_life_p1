3d07b366fcbf9ace659d5023daaea8a1865a3914
Fabs t8 <branzeafabian21@gmail.com>
2023-08-01 15:07:08 +0100

    Initializing routes

--- dogs-app-ui/src/App.js
+++ dogs-app-ui/src/App.js
@@ -3,22 +3,35 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NumberOfDogs from './components/NumberOfDogs'
+import AllDogs from './components/AllDogs'
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">Dogs</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">All Dogs</Nav.Link>
-            <Nav.Link href="#link">Number Of Dogs</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+
+          <Navbar.Brand href="#home">Dogs</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">All Dogs</Nav.Link>
+              <Nav.Link href="#link">Number Of Dogs</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/all" element={AllDogs} />
+        <Route path="/number" element={NumberOfDogs} />
+      </Routes>
+    </>
   );
+
 }
 
 export default App;
